d45a3d25f2c3a5ee92cb52442b2cae515571a91f
aymane-2 <iaatoto6@gmail.com>
2026-01-10 16:13:28 +0100

    Last test

--- flask-crud-frontend/App.test.js
+++ flask-crud-frontend/App.test.js
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
-import useItems from './hooks/useItems'; // Verify this path matches your file structure
+import useItems from './src/hooks/useItems'; // Verify this path matches your file structure
 
 // 1. Mock the entire module containing the hook
 jest.mock('./hooks/useItems');
